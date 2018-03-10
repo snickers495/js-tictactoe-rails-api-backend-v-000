ff15884f09b297e9231c9cf57d9a2d2e99a42fd3
Lisa Lee <snickers495@gmail.com>
2018-03-10 20:02:45 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
-    render(status: 201, json: Game.create(request.body.state))
+    render(status: 201, json: Game.create(state: .state))
   end
 
 
